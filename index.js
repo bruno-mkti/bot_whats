7be0a68a33eb7dc8ef7c46f180bba26ab8f43fe9
Bruno Silva <sbruno25@hotmail.com>
2025-02-12 13:55:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const qrcode = require('qrcode-terminal');
+const fs = require('fs');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
+// Configuração para armazenar a sessão localmente
+const client = new Client({
+    authStrategy: new LocalAuth() // Salva a sessão automaticamente
+});
 
 client.on('qr', qr => {
+    console.log("Escaneie o QR Code para autenticar:");
     qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
-    console.log('Client is ready!');
+    console.log('✅ Bot está pronto e conectado!');
 });
 
 client.on('message', message => {
     if (message.body.toLocaleLowerCase() === 'oi') {
-        client.sendMessage(message.from, 'Ola, eu sou o seu atendente virtual!');
+        client.sendMessage(message.from, 'Olá, eu sou o seu atendente virtual!');
         client.sendMessage(message.from, 'Sobre qual assunto deseja falar? Digite a opção desejada');
         client.sendMessage(message.from, '1. Salgados');
         client.sendMessage(message.from, '2. Doces');
@@ -41,6 +46,6 @@ client.on('message', message => {
         client.sendMessage(message.from, 'Refrigerante');
         client.sendMessage(message.from, 'Cerveja');
     }
-})
+});
 
 client.initialize();
